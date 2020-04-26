d44fb28a9e09580e95a3588add840ca5595630c6
ashssiv <ashsiv.s@gmail.com>
2020-04-26 15:01:13 -0700

    Added Generator functionality

--- model.py
+++ model.py
@@ -9,39 +9,55 @@ import math
 import cv2
 import numpy as np
 import sklearn
-from sklearn.model_selection import train_test_split
 
-
+## Import data path locations && do training/validation split
 samples = []
-with open('./mydata/driving_log.csv') as csvfile:
+with open('./mydata/mydata/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         samples.append(line)
 
-images = []
-angles=[]
-s=0.2
-count=0;
-steering_factor = [0.0, s,-s];
-for sample in samples[1:]:
-    print(count);
-    count=count+1;
-    for i in range(3):
-        name = './mydata/IMG/'+sample[i].split('/')[-1]
-        image = cv2.imread(name)
-        angle = float(sample[3])
-        images.append(image)
-        angles.append(angle+steering_factor[i])
-        images.append(cv2.flip(image,1))
-        angles.append((angle+steering_factor[i])*-1.0)
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+## Define generator function to load and preprocess data on the fly in batches
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
         
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            images = []
+            angles = []
+            s=0.2
+            count=0;
+            steering_factor = [0.0, s,-s];
+            for batch_sample in batch_samples:
+                #print(count);
+                count=count+1;
+                for i in range(3):
+                    name = './mydata/mydata/IMG/'+batch_sample[i].split('/')[-1]
+                    image = cv2.imread(name)
+                    angle = float(batch_sample[3])
+                    images.append(image)
+                    angles.append(angle+steering_factor[i])
+                    images.append(cv2.flip(image,1))
+                    angles.append((angle+steering_factor[i])*-1.0)
 
-X_train = np.array(images)
-y_train = np.array(angles)
- 
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)        
+        
+# Set our batch size
+batch_size=100       
 
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=batch_size)
+validation_generator = generator(validation_samples, batch_size=batch_size)
+                
+## Define the Model Architecture
 model = Sequential()
-model.add(Lambda(lambda x: (x/255.0) -0.5, input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x/255)-0.5, input_shape=(160,320,3))) # trim image to only see section with road
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu"))
 model.add(Conv2D(36, (5, 5), strides=(2, 2), activation="relu"))
@@ -55,5 +71,8 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 model.compile(optimizer="adam",loss="mse")
-model.fit(X_train, y_train, nb_epoch=3,validation_split=0.2,shuffle=True)
+model.fit_generator(train_generator,steps_per_epoch=math.ceil(len(train_samples)/batch_size),validation_data=validation_generator, validation_steps=math.ceil(len(validation_samples)/batch_size),epochs=3, verbose=1)
+
+## Save the model
 model.save('model.h5')
+model.summary()
